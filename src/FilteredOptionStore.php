602d60f87531d21ab92daebe79b37c25681d7bc5
Tang Rufus <tangrufus@gmail.com>
2017-10-17 02:58:29 +0800

    Add `FilteredOptionStore`

---
+++ src/FilteredOptionStore.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * WP Option Store
+ *
+ * A simplified OOP implementation of the WordPress Options API.
+ *
+ * @package   TypistTech\WPOptionStore
+ *
+ * @author    Typist Tech <wp-option-store@typist.tech>
+ * @copyright 2017 Typist Tech
+ * @license   GPL-2.0+
+ *
+ * @see       https://www.typist.tech/projects/wp-option-store
+ * @see       https://github.com/TypistTech/wp-option-store
+ */
+
+declare(strict_types=1);
+
+namespace TypistTech\WPOptionStore;
+
+class FilteredOptionStore extends OptionStore
+{
+    /**
+     * Option getter.
+     *
+     * @param string $optionName Name of option to retrieve.
+     *
+     * @return mixed|null Null if option not exists or its value is actually null.
+     */
+    public function get(string $optionName)
+    {
+        return apply_filters(
+            $this->filterTagFor($optionName),
+            parent::get($optionName)
+        );
+    }
+
+    /**
+     * Normalize option name and key to snake_case filter tag.
+     *
+     * @param string $optionName Name of option to retrieve.
+     *                           Expected to not be SQL-escaped.
+     *
+     * @return string
+     */
+    private function filterTagFor(string $optionName): string
+    {
+        return strtolower($optionName);
+    }
+}
